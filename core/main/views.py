f857d2faae784f07b1a16bdabb70a8c42a3e0d33
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-08-15 15:52:38 +0400

    calculator

---
+++ core/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def res(request):
+    num1 = int(request.POST['num1'])
+    num2 = int(request.POST['num2'])
+    char = request.POST['char']
+    if char == '+':
+        res = num1 + num2
+    elif char == '-':
+        res = num1 - num2
+    return render(request, 'res.html', {'res':res})
